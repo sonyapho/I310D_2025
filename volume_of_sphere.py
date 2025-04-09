28c4a72fcde9b6dacdf301af4c634a640d25f12f
Sonya Pholsiri <sonyapholsiri@wireless-10-155-248-5.public.utexas.edu>
2025-04-09 11:00:00 -0500

    added new file

---
+++ volume_of_sphere.py
@@ -0,0 +1,14 @@
+def calculate_volume_of_sphere(radius):
+    pi = 3.14
+    volume = (4/3)*pi*r^3
+    return volume
+
+radius1 = 30
+area1 = calculate_volume_of_sphere(radius1)
+print(f'The volume of the circle with radius {radius1} is {area1}.')
+
+
+radius2 = 40
+area2 = calculate_volume_of_sphere(radius2)
+print(f'The volume of the circle with radius {radius2} is {area2}.')
+
